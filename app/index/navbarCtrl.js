a9448fe52795dd913fac26ac5b77dca1e12dfa22
hualuomoli <liubaoquan0831@163.com>
2016-01-20 10:03:47 +0800

    remove temp demo file

--- app/index/navbarCtrl.js
+++
@@ -1,8 +0,0 @@
-define(function (require) {
-	var app = require('../app');
-
-	app.controller('navbarCtrl', ['$scope', function ($scope) {
-		$scope.nickName = 'Manager';
-	}]);
-
-});
--- app/index/pageCtrl.js
+++
@@ -1,8 +0,0 @@
-define(function (require) {
-	var app = require('../app');
-
-	app.controller('pageCtrl', ['$scope', function ($scope) {
-
-	}]);
-
-});
