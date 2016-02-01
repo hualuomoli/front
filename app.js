ec0cb5867959b044a7fe10dad9358c0d0b15a2e9
hualuomoli <liubaoquan0831@163.com>
2016-02-01 13:44:04 +0800

    update

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var path = require('path');
 var app = express();
 
 // 
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use('/app', express.static(path.join(__dirname, 'app')));
 app.use('/views', express.static(path.join(__dirname, 'views')));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.set('port', process.env.PORT || '3000');
 
--- publish.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd build
-node r.js -o build.js
-
-cd ..
-gulp pro
-
-pause
